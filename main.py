37e12bebaf0db951427bf4361fcd1771f5b39294
Yerlan Negmetulla <yerlann@ynMac.local>
2021-03-26 22:53:11 +0600

    add Post list

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
-from flask import Flask, request, session, render_template, redirect, url_for, make_response, jsonify
+from flask import Flask, g, request, session, render_template, redirect, url_for, make_response, jsonify
 import sqlite3
 import pandas as pd
 import hashlib
 
 
 app = Flask(__name__)
+
+def get_db():
+    if not hasattr(g, 'sqlite_db'):
+        g.sqlite_db = create_connection('../db.sqlite3')
+    return g.sqlite_db
 
 
 def create_connection(db_file):
@@ -31,21 +36,31 @@ def select_account(conn, account):
     return cur.fetchone()
 
 
-def sql_select(query):
-    try:
-        conn = sqlite3.connect('../db.sqlite3')
-        df = pd.read_sql_query(query, conn).to_dict('records')
-    except Exception as err:
-        print('Error while creating the connection ', err)
-    finally:
-        conn.close()
+def select_post(conn, post_id):
+    sql = ''' SELECT * FROM posts WHERE id=? '''
+    conn.row_factory = sqlite3.Row
+    cur = conn.cursor()
+    cur.execute(sql, (post_id,))
+    return dict(cur.fetchone())
+
+
+def sql_select(conn, query):
+    df = pd.read_sql_query(query, conn).to_dict('records')
     return df
 
 
 @app.route('/')
 def index():
-    posts = sql_select("SELECT * FROM posts")
+    conn = get_db()
+    posts = sql_select(conn, "SELECT * FROM posts ORDER BY id DESC")
     return render_template("main/home.html", posts=posts)
+
+
+@app.route('/p/<int:id>')
+def post_detail(id):
+    conn = get_db()
+    post = select_post(conn, id)
+    return render_template("main/post_detail.html", post=post)
 
 
 @app.route('/login', methods=['POST', 'GET'])
@@ -55,7 +70,7 @@ def login():
         password = request.form['password']
 
         err_msg = []
-        conn = create_connection("../test.db")
+        conn = get_db()
         account = (email, hashlib.sha1(password.encode()).hexdigest())
         if select_account(conn, account):
             return redirect("/")
@@ -78,7 +93,7 @@ def signup():
             err_msg.append("Password incorrect.")
 
         if not err_msg:
-            conn = create_connection("../test.db")
+            conn = get_db()
             with conn:
                 account = (username, email, hashlib.sha1(password.encode()).hexdigest())
                 try:
